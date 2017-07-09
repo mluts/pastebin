52802aff70b6f162887e64026ed88a6e4be5a623
James Mills <prologic@shortcircuit.net.au>
2017-07-09 10:18:41 -0700

    Removed some cruft

--- main.go
+++ main.go
@@ -4,26 +4,20 @@ import (
 	"log"
 	"time"
 
-	"github.com/boltdb/bolt"
 	"github.com/namsral/flag"
 )
 
-var (
-	db  *bolt.DB
-	cfg Config
-)
+var cfg Config
 
 func main() {
 	var (
 		config string
-		dbpath string
 		bind   string
 		fqdn   string
 		expiry time.Duration
 	)
 
 	flag.StringVar(&config, "config", "", "config file")
-	flag.StringVar(&dbpath, "dbpth", "urls.db", "Database path")
 	flag.StringVar(&bind, "bind", "0.0.0.0:8000", "[int]:<port> to bind to")
 	flag.StringVar(&fqdn, "fqdn", "localhost", "FQDN for public access")
 	flag.DurationVar(&expiry, "expiry", 5*time.Minute, "expiry time for pastes")
@@ -36,13 +30,6 @@ func main() {
 	// TODO: Abstract the Config and Handlers better
 	cfg.fqdn = fqdn
 	cfg.expiry = expiry
-
-	var err error
-	db, err = bolt.Open(dbpath, 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 
 	NewServer(bind, cfg).ListenAndServe()
 }
